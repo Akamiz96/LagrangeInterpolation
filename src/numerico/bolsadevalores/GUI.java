635e78f267b9da61415e86f1752736c62c53bf37
pabloarizaluna <pablo_ariza@javeriana.edu.co>
2017-02-18 03:50:36 -0500

    Projecto de netbean

---
+++ src/numerico/bolsadevalores/GUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerico.bolsadevalores;
+
+/**
+ *
+ * @author Ailin Rojas
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
